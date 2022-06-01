6147a9c4674767495ba9ac750a6409758847fa77
Ramón Eduardo <repb.osa@gmail.com>
2022-06-01 15:07:47 -0600

    2da version del gestionar Categoria
    Ahora ya puede seleccionar a que catalogo pertenece, etc

--- app/Http/Livewire/Categoria/Listado.php
+++ app/Http/Livewire/Categoria/Listado.php
@@ -10,11 +10,14 @@ use Livewire\WithPagination;
 class Listado extends Component
 {
     /* $categorias,  */
-    public $cataCatego, $search, $catalogosCategorias, $viendoCatalo;
+    public $cataCatego, $search, $catalogos, $catalogosCategorias, $viendoCatalo, $viendoCatalos, $arrrayCategoiasID,
+    $categoriaSelectec;
     use WithPagination;
     public function mount()
     {
         $this->viendoCatalo=false;
+        $this->viendoCatalos=false;
+        $this->arrrayCategoiasID=[];
     }
     public function render()
     {
@@ -29,6 +32,47 @@ class Listado extends Component
         $this->catalogosCategorias=CatalogoCategoria::where('categoria_id', $value)->get();
         $this->viendoCatalo=true;
         //dd($this->catalogosCategorias);
+    }
+    public function catalogos_gestionar($value)
+    {
+        $this->arrrayCategoiasID=[];
+        $this->categoriaSelectec=$value;
+        $this->catalogos= Catalogo::all();
+        $this->catalogosCategorias=CatalogoCategoria::where('categoria_id', $value)->get();
+        foreach ($this->catalogosCategorias as $key => $value) {
+            //array_push($this->arrrayCategoiasID, $value->id);
+            $this->arrrayCategoiasID[$value->catalogo_id]= $value->catalogo_id;
+        }
+        $this->viendoCatalos=true;
+    }
+    public function save_catalogos()
+    {
+        //dd($this->arrrayCategoiasID,$this->categoriaSelectec );
+        $this->catalogos= Catalogo::all();
+        $eliminar=true;
+        $this->catalogosCategorias=CatalogoCategoria::where('categoria_id', $this->categoriaSelectec)->get();
+        foreach ($this->catalogosCategorias as $key => $value) {
+            foreach ($this->arrrayCategoiasID as $keyy => $valuex) {
+                //dump($value->catalogo_id , "El bueno ". $valuex);
+                if ($value->catalogo_id == $valuex) {
+                    $eliminar=false;
+                    //break;
+                }
+                if ($value->catalogo_id !== $valuex && $valuex !==null)  {
+                    $newValue = CatalogoCategoria::create([
+                        'catalogo_id' => $valuex,
+                        'categoria_id' => $value->categoria_id,
+                    ]);
+                    $newValue->save();
+                }
+            }    
+            if ($eliminar==true) {
+                $value->delete();
+            }
+            //dump($eliminar);
+        }
+        
+        
     }
     public function edit($value)
     {
--- resources/views/livewire/categoria/listado.blade.php
+++ resources/views/livewire/categoria/listado.blade.php
@@ -23,7 +23,7 @@
                             <tr>
                                 <th scope="col">#</th>
                                 <th scope="col">Nombre</th>
-                                <th scope="col">Categorias</th>
+                                <th scope="col">Catálogos</th>
                                 <th scope="col">Acciones</th>
 
                             </tr>
@@ -33,15 +33,19 @@
                                 <tr>
                                     <th scope="row">{{ $key + 1 }}</th>
                                     <td>{{ $value->name }}</td>
-                                    <td> 
+                                    <td>
                                         <button type="button" class="btn btn-info" data-toggle="modal"
-                                        data-target="#exampleModal"
+                                            data-target="#exampleModal"
                                             wire:click="catalogos_Perte({{ $value->id }})">Ver</button>
                                     </td>
                                     <td>
-                                        <button type="button" class="btn btn-primary" 
+                                        <button type="button" class="btn btn-dark" data-toggle="modal"
+                                            data-target="#exampleModal2"
+                                            wire:click="catalogos_gestionar({{ $value->id }})">
+                                            Catálogos</button>
+                                        <button type="button" class="btn btn-primary"
                                             wire:click="edit({{ $value->id }})">Editar</button>
-                                        <button type="button" class="btn btn-danger" 
+                                        <button type="button" class="btn btn-danger"
                                             wire:click="delete({{ $value->id }})">Eliminar</button>
                                     </td>
 
@@ -62,7 +66,6 @@
 
 
     {{-- MODAL --}}
-    
     <div class="modal fade" id="exampleModal" wire:ignore.self tabindex="-1" role="dialog"
         aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
@@ -77,10 +80,10 @@
                     <form>
                         <div class="form-group">
                             {{-- <label for="recipient-name" class="col-form-label">Nombre:</label> --}}
-                            @if($viendoCatalo)
-                            @foreach($catalogosCategorias as $key => $value)
-                                <h5>{{ $value->catalogo->name }}</h5>    
-                            @endforeach
+                            @if ($viendoCatalo)
+                                @foreach ($catalogosCategorias as $key => $value)
+                                    <h5>{{ $value->catalogo->name }}</h5>
+                                @endforeach
                             @endif
                         </div>
                         {{-- <div class="form-group">
@@ -91,12 +94,49 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-                    
+
                 </div>
             </div>
         </div>
     </div>
-    
+    {{-- MODAL --}}
+    <div class="modal fade" id="exampleModal2" wire:ignore.self tabindex="-1" role="dialog"
+        aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Editando</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="form-group">
+                            @if ($viendoCatalos)
+                                @foreach ($catalogos as $key => $value)
+                                    {{-- <h5>{{ $value->catalogo->name }}</h5> --}}
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-check-input" id=""
+                                        value="{{ $value->id }}"
+                                        wire:model="arrrayCategoiasID.{{ $value->id }}">
+                                        <label class="form-check-label" for="exampleCheck1">{{ $value->name }}</label>
+                                    </div>
+                                    
+                                @endforeach
+                            @endif
+
+                        </div>
+
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                    <button type="button" class="btn btn-primary" wire:click="save_catalogos()">Guardar</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 </div>
