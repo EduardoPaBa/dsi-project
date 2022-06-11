b9e6879ef42747dcf9d4d72deb266ee013018d83
Ramón Eduardo <repb.osa@gmail.com>
2022-06-11 15:51:52 -0600

    cascaron de el AgregarCategoria
    no esta funcional, solo se hizo la vista, humilde uno

---
+++ app/Http/Livewire/Categoria/Agregar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Categoria;
+
+use Livewire\Component;
+
+class Agregar extends Component
+{
+    public function render()
+    {
+        return view('livewire.categoria.agregar');
+    }
+}
---
+++ resources/views/livewire/categoria/agregar.blade.php
@@ -0,0 +1,25 @@
+{{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+<div class="" style="float: center; margin: 1rem; ">
+    {{-- Do your work, then step back. --}}
+    <br><br>
+    {{-- <h1>hellou</h1> --}}
+    <div class="row">
+        <div class="col">
+        </div>
+        <div class="col-8">
+            <div class="card">
+                <div class="card-header">
+                    GESTIONAR CATEGORÍA
+                </div>
+
+                <div class="card-body">
+                    <a href="{{ route('listCategor') }}"> <button type="button" class="btn btn-outline-secondary"
+                        style="float: right;">Volver</button> </a> <br>
+                        <h5>Humilde en produccion</h5>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+        </div>
+    </div>
+</div>
