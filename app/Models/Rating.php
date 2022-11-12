7b15a4a39676c344d8ec5a56d591121169222418
dag-98 <daguilar.pacheco98@gmail.com>
2022-11-11 19:40:14 -0600

    modelo y migracion para rating de productos

---
+++ app/Models/Rating.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rating extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+
+    protected $fillable = [
+        
+        'usuario_id',
+        'producto_id',
+        'rating',
+        'comment',
+        'status',
+    ];
+
+    public function usuario()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function producto()
+    {
+        return $this->belongsTo(Producto::class);
+    }
+}
